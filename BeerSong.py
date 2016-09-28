c76f8969b845e9138532aec6557c8b1272d36bee
tdownie143 <tdownie143@gmail.com>
2016-09-28 17:58:10 -0400

    Beer song sudocode added. main method and ninety nine beer method

---
+++ BeerSong.py
@@ -0,0 +1,7 @@
+main():
+	calls beer function
+
+Ninety_Nine_Bottles_Of_Beer():
+	loop from 99 to 1
+	if x is greater than 1 output the beer song x bottles of beer on the wall x bottles of beer take one down pass it around x-1 bottles of beer
+	if x is == 1 then output last line of beer song
